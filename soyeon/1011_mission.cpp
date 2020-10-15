df52cc73ab375d24b9e6025883738fc88a422d35
ssom54 <71778256+ssom54@users.noreply.github.com>
2020-10-15 22:19:42 +0900

    1011 미션 (소연)

---
+++ soyeon/1011_mission.cpp
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+
+	while (1){
+		scanf("%d %d", &a, &b);
+		if (a !=0 || b != 0)
+			printf("%d\n", a + b); 
+		else
+			break;
+	}
+	return 0;
+}
+int solve() {
+	int n, i;
+	scanf("%d", &n);
+
+	for (i = 1; i < 10; i++) 
+		printf("%d * %d = %d\n", n, i, n * i);
+
+	return 0;
+}
+int solve()
+{
+	int x, y, quadrant;
+	scanf("%d", &x);
+	scanf("%d", &y);
+
+	if (x > 0) {
+		if (y > 0)
+			quadrant = 1;
+		else
+			quadrant = 4;
+	}
+	else {
+		if (y > 0)
+			quadrant = 2;
+		else
+			quadrant = 3;
+	}
+	printf("%d", quadrant);
+
+	return 0;
+}
+int solve()
+{
+	int n, i, j;
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= (n - i); j++)
+			printf(" ");
+		for (j = 1; j <= i; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
+int solve()
+{
+	int h, m;
+	scanf("%d %d", &h, &m);
+
+	if (m < 45) {
+		if (h == 0)
+			h = 23;
+		else
+			h -= 1;
+		m += 15;
+	}
+	else
+		m -= 45;
+
+	printf("%d %d", h, m);
+	return 0;
+}
